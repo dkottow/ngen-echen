507fe456e0a879ac9297e241ae3019c401ff1ed5
Daniel Kottow <dkottow@gmail.com>
2015-09-03 00:26:18 -0300

    build from src/ using gulp

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+
+gulp.task('default', ['build-3rdparty-js', 
+					'build-app-js', 
+					'build-3rdparty-css', 
+					'build-app-css',
+					'build-html'], function() {
+});
+
+//TODO
+gulp.task('copy-images', function() {
+});
+gulp.task('copy-fonts', function() {
+});
+
+gulp.task('build-3rdparty-css', function() {
+	return gulp.src(['./src/3rdparty/bootstrap/css/bootstrap.min.css', 
+					'./src/3rdparty/jquery-ui/jquery-ui.min.css', 
+					'./src/3rdparty/font-awesome/css/font-awesome.min.css', 
+					'./src/3rdparty/DataTables/media/css/jquery.dataTables.min.css', 
+				])
+
+		.pipe(concat('3rdparty.css'))
+		.pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('build-3rdparty-js', function() {
+	return gulp.src(['./src/3rdparty/jquery/jquery.min.js', 
+					'./src/3rdparty/jquery-ui/jquery-ui.min.js', 
+					'./src/3rdparty/DataTables/media/js/jquery.dataTables.min.js', 
+					'./src/3rdparty/underscore/underscore.js', 
+					'./src/3rdparty/backbone/backbone.js', 
+					'./src/3rdparty/bootstrap/js/bootstrap.min.js', 
+				])
+
+		.pipe(concat('3rdparty.js'))
+		.pipe(gulp.dest('./public/js/'));
+			
+});
+
+gulp.task('build-app-css', function() {
+});
+
+gulp.task('build-app-js', function() {
+	return gulp.src(["./src/js/models/**/*.js",
+					 "./src/js/collections/**/*.js",
+					 "./src/js/views/**/*.js",
+					 "./src/js/*.js",
+			])
+
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./public/js/'));
+			
+});
+
+
+
+gulp.task('build-html', function() {
+  // place code for your default task here
+	return gulp.src(['./src/index.html'])
+
+		.pipe(inject(gulp.src('./src/nav.html'), {
+		    starttag: '<!-- inject:nav:{{ext}} -->',
+		    transform: function (filePath, file) {
+		      return file.contents.toString('utf8')
+    		}
+  		}))
+
+		.pipe(inject(gulp.src(['./src/templates/*.html']), {
+		    starttag: '<!-- inject:templates:{{ext}} -->',
+		    transform: function (filePath, file) {
+		      return file.contents.toString('utf8')
+    		}
+  		}))
+		
+		.pipe(gulp.dest('./public/'));
+});
