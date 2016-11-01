79d1c2b484072814209b8b9b6024f6987e4ef7e7
Daniel <dkottow@gmail.com>
2016-11-01 20:09:24 +0000

    samples rentals ready for some testing..

--- gulpfile.js
+++ gulpfile.js
@@ -485,7 +485,7 @@ gulp.task('watch', function() {
     gulp.watch('./src/schema.html', ['build-schema-html']);
     gulp.watch('./src/css/*.css', ['build-donkeylift-css']);
     gulp.watch('./src/docs/*', ['build-api-json']);
-    gulp.watch('./src/docs/samples/*', ['build-samples']);
+    gulp.watch('./src/docs/samples/*', ['build-samples', 'build-samples-html']);
     gulp.watch('./src/signup.html', ['build-signup-html']);
     gulp.watch('./src/index.html', ['build-index-html']);
 });
