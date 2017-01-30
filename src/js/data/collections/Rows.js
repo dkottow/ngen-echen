7965a8c8cd523827c0f1b4c853b639c0baee5e31
Daniel <dkottow@gmail.com>
2017-01-30 01:50:06 +0000

    .

---
+++ src/js/data/collections/Rows.js
@@ -0,0 +1,11 @@
+/*global Backbone, Donkeylift */
+
+// Tables Collection
+// ---------------
+
+Donkeylift.Rows = Backbone.Collection.extend({
+	// Reference to this collection's model.
+	model: Donkeylift.Row,
+
+});
+
---
+++ src/js/data/models/Row.js
@@ -0,0 +1,9 @@
+/*global Donkeylift, Backbone, _ */
+
+Donkeylift.Row = Backbone.Model.extend({ 
+
+	initialize : function(attrs) {
+		console.log("Row.initialize " + attrs.id);
+	},
+
+});		
