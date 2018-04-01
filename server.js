c61c08cc826da271de173751a1f34ac8fca6d3fe
unknown <dkottow@gmail.com>
2018-04-01 18:21:52 -0300

    api controller...

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ var path = require('path');
 var util = require('util');
 
 var app = express();
+var ApiController = require('./app/ApiController').ApiController;
 
 //require('dotenv').config();
 var config = require('config');
 
+//all api routes
+controller = new ApiController();
+app.use('/api', controller.router);
+
+//just for debugging, Azure will use IIS static handler
 app.use('/', express.static(path.resolve(__dirname, 'public'))); 
+
 
 app.listen(config.port, config.ip, function() {
 	console.log("Started server on " + config.ip + ":" + config.port);
